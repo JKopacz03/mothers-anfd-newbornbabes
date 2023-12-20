90ad0529806b4889cfb473cf64b170aa0de0d07f
Jarosław Kopacz <ignacyikopacz@gmail.com>
2023-12-20 12:29:38 +0100

    Create Mother service

---
+++ src/main/java/com/kopacz/mothersandnewbornbabes/mother/MotherService.java
@@ -0,0 +1,14 @@
+package com.kopacz.mothersandnewbornbabes.mother;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MotherService {
+
+    private final MotherRepository motherRepository;
+
+
+
+}
