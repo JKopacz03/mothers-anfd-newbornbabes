389938fb58a7f15cc60387ba96a039c9e99fb5b6
Jarosław Kopacz <ignacyikopacz@gmail.com>
2023-12-20 11:46:20 +0100

    Init commit

---
+++ src/main/java/com/kopacz/mothersandnewbornbabes/MothersAndNewbornbabesApplication.java
@@ -0,0 +1,13 @@
+package com.kopacz.mothersandnewbornbabes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MothersAndNewbornbabesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MothersAndNewbornbabesApplication.class, args);
+	}
+
+}
